3d5d975b2119e4e6ed5f52af8926d5e2393ee595
Renan Morais <renanmorais@live.com>
2020-11-23 15:30:44 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,27 @@
+// const axios = require('axios');
+let output = document.querySelector('.output');
+
+function search(){
+    let input = document.querySelector('.input').value;
+    output.innerHTML = `
+        Carregando...
+    `;
+    axios.get('https://api.github.com/users/' + input)
+    .then(response => {
+        output.innerHTML = `
+            Login: ${response.data.login}<br>
+            Avatar: <img src="${response.data.avatar_url}"><br>
+            Nome: ${response.data.name}<br>
+            Biografia: ${response.data.bio}<br>
+            Github: <a href="${response.data.html_url}">${response.data.html_url}</a><br>
+            ID: ${response.data.id}<br>
+            Localização: ${response.data.location}
+        `;
+    }).catch(error => {
+        if (error.response.status === 404) {
+            output.innerHTML = `
+                Usuário não encontrado
+            `;
+        }
+    });
+};
