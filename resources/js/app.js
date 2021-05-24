a603cd6dd9c3a2b5639f1b6978727bb67f1bc473
Renan Morais <renan.morais@multicredito.net.br>
2021-05-24 15:38:03 -0300

    Inserido conteudo em pt-br

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,7 @@ let search = document.querySelector('.search');
 
 search.onclick = function(){
     let input = document.querySelector('#input').value;
+
     output.innerHTML = `
         <div class="text-center">
             <div class="spinner-border text-dark mx-auto" role="status">
@@ -13,17 +14,19 @@ search.onclick = function(){
             </div>
         </div>
     `;
+
     search.disabled = true;
+    
     axios.get('https://api.github.com/users/' + input)
     .then(response => {
         output.innerHTML = `
             <img src="${response.data.avatar_url}" class="rounded-circle d-block mx-auto mb-3 img-fluid">
             <p>            
-                <b>User Name:</b> ${response.data.login}<br>
-                <b>Name:</b> ${response.data.name}<br>
-                <b>Biography:</b> ${response.data.bio}<br>
-                <b>Github:</b> <a href="${response.data.html_url}" target="_blank">${response.data.html_url}</a><br>
-                <b>Location:</b> ${response.data.location}
+                <b>Usuário:</b> ${response.data.login}<br>
+                <b>Nome:</b> ${response.data.name}<br>
+                <b>Resumo:</b> ${response.data.bio}<br>
+                <b>GitHub:</b> <a href="${response.data.html_url}" target="_blank">${response.data.html_url}</a><br>
+                <b>Localização:</b> ${response.data.location}
             </p>
         `;
         search.disabled = false;
